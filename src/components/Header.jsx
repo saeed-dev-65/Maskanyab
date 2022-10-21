7786318c6a34fa5319ce72c3deb6fb77ed9ce73d
saeed kavand <mr.kavand@gmail.com>
2022-10-21 17:56:20 +0330

    create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,66 @@
+import { useNavigate, useLocation } from 'react-router-dom';
+import React from 'react';
+
+const Header = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+    function activeLink(link) {
+        if (link === location.pathname) {
+            return true;
+        }
+    }
+    return (
+        <div className="bg-white border-b shadow-sm sticky top-0 z-50">
+            <header
+                className={
+                    'flex justify-between items-center px-3 max-w-6xl mx-auto'
+                }
+            >
+                <div>
+                    <img
+                        src="./images/logo.svg"
+                        className="h-8 cursor-pointer"
+                        alt="Maskan-Yab logo"
+                        draggable={false}
+                        onClick={() => navigate('/')}
+                    />
+                </div>
+                <nav>
+                    <ul className="flex space-x-10 rtl:space-x-reverse">
+                        <li
+                            className={`cursor-pointer text-sm font-semibold text-gray-400 py-3 border-b-[3px] border-transparent ${
+                                activeLink('/')
+                                    ? 'text-black border-red-500'
+                                    : ''
+                            }`}
+                            onClick={() => navigate('/')}
+                        >
+                            صفحه اصلی
+                        </li>
+                        <li
+                            className={`cursor-pointer text-sm font-semibold text-gray-400 py-3 border-b-[3px] border-transparent ${
+                                activeLink('/offers')
+                                    ? 'text-black border-red-500'
+                                    : ''
+                            }`}
+                            onClick={() => navigate('/offers')}
+                        >
+                            پیشنهادها
+                        </li>
+                        <li
+                            className={`cursor-pointer text-sm font-semibold text-gray-400 py-3 border-b-[3px] border-transparent ${
+                                activeLink('/sign-in')
+                                    ? 'text-black border-red-500'
+                                    : ''
+                            }`}
+                            onClick={() => navigate('/sign-in')}
+                        >
+                            ورود
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    );
+};
+export default Header;
